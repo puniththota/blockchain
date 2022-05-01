e248a1d5cefc8c15911b6266017c381deadaa8f6
punith <puniththota1996@gmail.com>
2022-05-01 17:47:53 +0100

    Flask app and mine_block end point added

--- module_1/blockchain.py
+++ module_1/blockchain.py
@@ -54,3 +54,25 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+    
+#2 mining
+#creating webapp
+app = Flask(__name__)
+
+#creating blockchain
+blockchain = Blockchain()
+
+#mining a new block
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    new_block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'block_index': block['index'],
+                'block_timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(respone), 200

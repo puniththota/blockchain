3780c1ec87f763cfea8dd129125257ac4884530a
punith <puniththota1996@gmail.com>
2022-05-01 18:11:03 +0100

    is valid end point added

--- module_1/blockchain.py
+++ module_1/blockchain.py
@@ -85,5 +85,11 @@ def get_chain():
                 'chain_length': len(blockchain.chain)}
     return jsonify(response), 200
 
+#is chain valid endpoint
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    response = {'valid': blockchain.is_chain_valid(blockchain.chain)}
+    return jsonify(response), 200
+
 #running the app
 app.run(host = '0.0.0.0', port = 5000)
